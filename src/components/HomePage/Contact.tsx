134898769b151aecd703eed89c93aca609fd0725
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-03-12 23:40:59 +0600

    fix: fix bug

--- src/components/HomePage/Contact.tsx
+++ src/components/HomePage/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div id="contact" className="max-w-7xl mx-auto px-5 pt-16 sm:pt-20 pb-8">
       <h1 className=" text-center text-2xl sm:text-3xl font-bold text-purple-800">
-        We're Here to Help—Get in Touch
+        {` We're Here to Help—Get in Touch`}
       </h1>
       <div className=" max-w-screen-md mx-auto my-5">
         <ContactForm />
