e93c4fb02de3a1a9111b31a2440856ecc6a9ee26
Md Shobuj Mia <md.shobuj.developer@gmail.com>
2025-03-17 01:01:02 +0600

    updated code

--- src/components/BookCall.tsx
+++ src/components/BookCall.tsx
@@ -16,7 +16,7 @@ const BookCall = () => {
     <>
       <button
         onClick={handleClick}
-        className=" fixed sm:bottom-14 bottom-12   z-30  right-5 flex items-center gap-2 text-white bg-purple-800 sm:p-3 p-2 rounded-md cursor-pointer"
+        className=" fixed sm:bottom-14 bottom-12   z-50  right-5 flex items-center gap-2 text-white bg-purple-800 sm:p-3 p-2 rounded-md cursor-pointer"
       >
         <FiCalendar size={18} />
         <span className=" sm:text-sm text-xs  font-medium">Book A Call</span>
@@ -32,7 +32,7 @@ const BookCall = () => {
         </span>
       </a>
       {isModalOpen && (
-        <div className=" fixed z-40 w-screen h-screen top-14 bottom-0 left-0 right-0 bg-[#0F0F0F]">
+        <div className=" fixed z-50 w-screen h-screen top-14 bottom-0 left-0 right-0 bg-[#0F0F0F]">
           <button
             onClick={() => setIsModalOpen(false)}
             className=" text-white absolute right-8 top-5 hover:bg-white transition-all hover:text-purple-700 bg-gray-800 p-2 rounded-full"
@@ -51,8 +51,8 @@ const BookCall = () => {
             </div>
           )}
           <iframe
-            src="https://cal.com/sk-shobuj-rumwsu"
-            className=" w-[90vw] mx-auto h-[90vh] mt-2"
+            src="https://cal.com/b2b-leadbazz"
+            className=" w-[90vw] mx-auto h-[90vh] mt-2 z-50"
             onLoad={() => setIsLoading(false)}
           ></iframe>
         </div>
